6f14aced7ed71f1ab05d48b66424c67447c02af6
Henrique GC <henriq4net@gmail.com>
2023-08-15 01:12:25 -0300

    criando regra de negocio para criacao das listas

---
+++ lista.c
@@ -0,0 +1,15 @@
+#include "lista.h"
+
+void insertProducts(int tamanho) {
+  char instrucao[] = "PRODUTO";
+  char produto_nome[] = "ALPISTE";
+  int produto_id = 4;
+  int data[3] = {27, 12, 1990};
+
+  for (int preco = tamanho; preco > 0; preco--) {
+    fprintf(file, "%s %s %d %d %d %d %d\n", instrucao, produto_nome, produto_id,
+            preco, data[0], data[1], data[2]);
+  }
+}
+
+void insertSortedList() { fprintf(file, "ORDENA_LISTA_VALOR\nFIM\n"); }
