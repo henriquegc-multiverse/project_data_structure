567c79f09279c7841cc6fc3cb6f41dcb26ac8708
Henrique GC <henriq4net@gmail.com>
2023-08-26 00:58:49 -0300

    fix: adicionando elementos em uma lista de forma orndenada (para o sort ordenar com o pior caso)

--- lista.c
+++ lista.c
@@ -6,7 +6,7 @@ void insertProducts(int tamanho) {
   int produto_id = 4;
   int data[3] = {27, 12, 1990};
 
-  for (int preco = tamanho; preco > 0; preco--) {
+  for (int preco = 0; preco < tamanho; preco++) {
     fprintf(file, "%s %s %d %d %d %d %d\n", instrucao, produto_nome, produto_id,
             preco, data[0], data[1], data[2]);
   }
